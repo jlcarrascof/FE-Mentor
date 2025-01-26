7e809bb72db1171c9d52ba34988162676429ba88
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-26 19:54:42 -0400

    Implement toggle functionality for FAQ answers and update icon based on visibility

--- 02-FAQ-ACCORDION/scripts.js
+++ 02-FAQ-ACCORDION/scripts.js
@@ -2,4 +2,16 @@ document.querySelectorAll('.faq-question').forEach(button => {
     button.addEventListener('click', () => {
         const answer = button.nextElementSibling;
         const icon = button.querySelector('.icon');
+
+        answer.classList.toggle('open');
+
+        if (answer.classList.contains('open')) {
+            icon.src = 'assets/images/icon-minus.svg';
+            button.setAttribute('aria-expanded', 'true');
+        } else {
+            icon.src = 'assets/images/icon-plus.svg';
+            button.setAttribute('aria-expanded', 'false');
+        }
+
+    });
 });
