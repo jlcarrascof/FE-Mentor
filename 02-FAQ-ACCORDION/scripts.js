82725079d6e169e678337724e0d9278e2f37f2be
jlcarrascof <entrenamientospuntonet@gmail.com>
2025-01-26 19:53:25 -0400

    Add functionality to toggle FAQ answer visibility and update icon on click

--- 02-FAQ-ACCORDION/scripts.js
+++ 02-FAQ-ACCORDION/scripts.js
@@ -1,4 +1,5 @@
 document.querySelectorAll('.faq-question').forEach(button => {
     button.addEventListener('click', () => {
-
+        const answer = button.nextElementSibling;
+        const icon = button.querySelector('.icon');
 });
